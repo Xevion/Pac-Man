274404b9eaf590960bb42fc4bd52e2263893fc8f
Xevion <xevion@xevion.dev>
2023-09-08 22:48:59 -0500

    feat: entity trait, direction enum (util)

---
+++ src/entity.rs
@@ -0,0 +1,8 @@
+pub trait Entity {
+    // Returns true if the entity is colliding with the other entity
+    fn is_colliding(&self, other: &dyn Entity) -> bool;
+    // Returns the absolute position of the entity
+    fn position(&self) -> (i32, i32);
+    // Returns the cell position of the entity (XY position within the grid)
+    fn cell_position(&self) -> (u32, u32);
+}
