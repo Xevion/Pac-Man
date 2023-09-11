03b2c5a6592e064d97820ceb49163e8a0fd1d805
Xevion <xevion@xevion.dev>
2023-09-11 17:14:10 -0500

    ci: cleanup build script

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -eux
 
+
+echo "Building WASM with Emscripten"
 cargo build --target=wasm32-unknown-emscripten --release
 
+echo "Copying release files to dist/"
 mkdir -p dist
-
-cp target/wasm32-unknown-emscripten/release/pacman.wasm dist
-cp target/wasm32-unknown-emscripten/release/pacman.js dist
 mkdir -p dist/deps
-cp target/wasm32-unknown-emscripten/release/deps/pacman.data dist/deps
+
+output_folder="target/wasm32-unknown-emscripten/release"
+cp $output_folder/pacman.wasm dist
+cp $output_folder/pacman.js dist
+cp $output_folder/deps/pacman.data dist/deps
 cp assets/index.html dist
