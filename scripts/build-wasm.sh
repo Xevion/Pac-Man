538cf1efb51cfcb102ff47b879343a63672d4619
Xevion <xevion@xevion.dev>
2023-09-11 18:27:25 -0500

    chore: copy .data file directly, remove locateFile step

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 set -eux
 
-
 echo "Building WASM with Emscripten"
 cargo build --target=wasm32-unknown-emscripten --release
 
 echo "Copying release files to dist/"
 mkdir -p dist
-mkdir -p dist/deps
 
 output_folder="target/wasm32-unknown-emscripten/release"
 cp $output_folder/pacman.wasm dist
 cp $output_folder/pacman.js dist
-cp $output_folder/deps/pacman.data dist/deps
+cp $output_folder/deps/pacman.data dist
 cp assets/index.html dist
