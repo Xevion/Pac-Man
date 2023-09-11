2c6045aa1b812ec07b0f028d658824e4c04d3711
Xevion <xevion@xevion.dev>
2023-09-11 03:01:50 -0500

    feat: add map struct, overhaul stored map representation

--- src/constants.rs
+++ src/constants.rs
@@ -1,11 +1,11 @@
-use lazy_static::lazy_static;
-
 pub const BOARD_WIDTH: u32 = 28;
-pub const BOARD_HEIGHT: u32 = 37; // Adjusted to fit map texture?
+pub const BOARD_HEIGHT: u32 = 31; // Adjusted to fit map texture?
 pub const CELL_SIZE: u32 = 24;
 
+pub const BOARD_OFFSET: (u32, u32) = (0, 3); // Relative cell offset for where map text / grid starts
+
 pub const WINDOW_WIDTH: u32 = CELL_SIZE * BOARD_WIDTH;
-pub const WINDOW_HEIGHT: u32 = CELL_SIZE * BOARD_HEIGHT;
+pub const WINDOW_HEIGHT: u32 = CELL_SIZE * (BOARD_HEIGHT + 6); // Map texture is 6 cells taller (3 above, 3 below) than the grid
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum MapTile {
@@ -17,9 +17,6 @@ pub enum MapTile {
 }
 
 pub const RAW_BOARD: [&str; BOARD_HEIGHT as usize] = [
-    "                            ",
-    "                            ",
-    "                            ",
     "############################",
     "#............##............#",
     "#.####.#####.##.#####.####.#",
@@ -51,45 +48,4 @@ pub const RAW_BOARD: [&str; BOARD_HEIGHT as usize] = [
     "#.##########.##.##########.#",
     "#..........................#",
     "############################",
-    "                            ",
-    "                            ",
-    "                            ",
 ];
-
-lazy_static! {
-    pub static ref BOARD: [[MapTile; BOARD_HEIGHT as usize]; BOARD_HEIGHT as usize] = {
-        let mut board = [[MapTile::Empty; BOARD_HEIGHT as usize]; BOARD_HEIGHT as usize];
-
-        for y in 0..BOARD_HEIGHT as usize {
-            let line = RAW_BOARD[y];
-
-            for x in 0..BOARD_WIDTH as usize {
-                if x >= line.len() {
-                    break;
-                }
-                
-                let i = (y * (BOARD_WIDTH as usize) + x) as usize;
-                let character = line
-                    .chars()
-                    .nth(x as usize)
-                    .unwrap_or_else(|| panic!("Could not get character at {} = ({}, {})", i, x, y));
-
-                let tile = match character {
-                    '#' => MapTile::Wall,
-                    '.' => MapTile::Pellet,
-                    'o' => MapTile::PowerPellet,
-                    ' ' => MapTile::Empty,
-                    c @ '0' | c @ '1' | c @ '2' | c @ '3' | c @ '4' => {
-                        MapTile::StartingPosition(c.to_digit(10).unwrap() as u8)
-                    },
-                    '=' => MapTile::Empty,
-                    _ => panic!("Unknown character in board: {}", character),
-                };
-
-                board[x as usize][y as usize] = tile;
-            }
-        }
-
-        board
-    };
-}
